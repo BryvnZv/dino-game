9dc076f19c6f2a2b80140a3ecb3883ff606f5336
BryvnZv <bryvnzv@gmail.com>
2023-03-14 19:38:23 -0400

    added start and stop game logic along with losing game logic and full functionality

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-import { updatedGround } from "./ground.js";
+import { updateGround, setupGround } from "./ground.js";
+import { updateDino, setupDino, getDinoRect, setDinoLose } from "./dino.js";
+import { updateCactus, setupCactus, getCactusRects } from "./cactus.js";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE = 0.00001;
 
 const worldElem = document.querySelector("[data-world]");
+const scoreElem = document.querySelector("[data-score]");
+const startScreenElem = document.querySelector("[data-start-screen]");
 
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
+document.addEventListener("keydown", handleStart, { once: true });
 
 let lastTime;
 let speedScale;
 let score;
-
 function update(time) {
   if (lastTime == null) {
     lastTime = time;
@@ -31,7 +36,47 @@ function update(time) {
   window.requestAnimationFrame(update);
 }
 
-window.requestAnimationFrame(update);
+function checkLose() {
+  const dinoRect = getDinoRect();
+  return getCactusRects().some((rect) => isCollision(rect, dinoRect));
+}
+
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left < rect2.right &&
+    rect1.top < rect2.bottom &&
+    rect1.right > rect2.left &&
+    rect1.bottom > rect2.top
+  );
+}
+
+function updateSpeedScale(delta) {
+  speedScale += delta * SPEED_SCALE_INCREASE;
+}
+
+function updateScore(delta) {
+  score += delta * 0.01;
+  scoreElem.textContent = Math.floor(score);
+}
+
+function handleStart() {
+  lastTime = null;
+  speedScale = 1;
+  score = 0;
+  setupGround();
+  setupDino();
+  setupCactus();
+  startScreenElem.classList.add("hide");
+  window.requestAnimationFrame(update);
+}
+
+function handleLose() {
+  setDinoLose();
+  setTimeout(() => {
+    document.addEventListener("keydown", handleStart, { once: true });
+    startScreenElem.classList.remove("hide");
+  }, 100);
+}
 
 function setPixelToWorldScale() {
   let worldToPixelScale;
