6badf9f9dc61a43f3acbdfac117b790fb637907a
BryvnZv <bryvnzv@gmail.com>
2023-03-14 19:04:28 -0400

    added in the styles and a helper file to assist with the development

---
+++ script.js
@@ -0,0 +1,46 @@
+import { updatedGround } from "./ground.js";
+
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 30;
+
+const worldElem = document.querySelector("[data-world]");
+
+setPixelToWorldScale();
+window.addEventListener("resize", setPixelToWorldScale);
+
+let lastTime;
+let speedScale;
+let score;
+
+function update(time) {
+  if (lastTime == null) {
+    lastTime = time;
+    window.requestAnimationFrame(update);
+    return;
+  }
+  const delta = time - lastTime;
+
+  updateGround(delta, speedScale);
+  updateDino(delta, speedScale);
+  updateCactus(delta, speedScale);
+  updateSpeedScale(delta);
+  updateScore(delta);
+  if (checkLose()) return handleLose();
+
+  lastTime = time;
+  window.requestAnimationFrame(update);
+}
+
+window.requestAnimationFrame(update);
+
+function setPixelToWorldScale() {
+  let worldToPixelScale;
+  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH;
+  } else {
+    worldToPixelScale = window.innerHeight / WORLD_HEIGHT;
+  }
+
+  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
+  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
+}
